3f69203be23f8b9f71e17b868d4ea978446b9f51
=FEATHERSTONE Nick <feathern@lcd.colorado.edu>
2015-12-11 14:15:48 -0700

    Added level 0 analytics (volume information) to IntervalData

--- stream/IntervalData.py
+++ stream/IntervalData.py
@@ -25,7 +25,14 @@ class IntervalData:
         Data modeling for each interval we have configured to process
         Intervals are defined by the
     '''
-    
+
+    #Level 0 analytics:  volume information
+    maxvol  = 0 # max volume over this interval
+    minvol  = 0 # min volume over this interval
+    meanvol = 0 # mean volume over this interval
+    volran  = 0 # just maxvol - minvol
+    stdvol  = 0 # standard deviation of volume over this interval
+
     data = [1]
     ''' list
     List of data
@@ -56,26 +63,34 @@ class IntervalData:
 #         for d in np.nditer(data):
 #             print d
 #         sys.exit(1)
-        
+
+        #We have a series of analytics_levels that are increasingly complex
+        # Analytics level N may depend on levels 0 through N-1, and so
+        # we use the logic below with >=  and NOT ==.  This prevents
+        # redundant code (i.e. level 0 analyses appearing in the level 1 branch)        
         if analytics_level == -1:
             # RUN LEVEL -1
             pass
-        if analytics_level == 0:
-            # RUN LEVEL 0
-            pass
+        if analytics_level >= 0:
+            volume = np.abs(data)
+            self.maxvol  = np.max(volume)
+            self.minvol  = np.min(volume)
+            self.meanvol = np.mean(volume)
+            self.volran = self.maxvol-self.minvol
+            self.stdvol = np.std(volume)
             
-        if analytics_level == 1:
+        if analytics_level >= 1:
             # RUN LEVEL 1
             pass
             
-        if analytics_level == 2:
+        if analytics_level >= 2:
             # RUN LEVEL 2
             pass
             
-        if analytics_level == 3:
+        if analytics_level >= 3:
             # RUN LEVEL 3
             pass
             
-        if analytics_level == 4:
+        if analytics_level >= 4:
             # RUN LEVEL 4
             pass
